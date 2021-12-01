f8fca7c317f68983d319cc8748e68080e8cf1d5d
James Johnson <james.johnson@focusrite.com>
2021-12-01 12:22:47 +0000

    Test two measurements in sonar sweep

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -4,7 +4,13 @@ fn read_input() -> String {
     return fs::read_to_string("./src/day1/input.txt").unwrap();
 }
 
-fn count_increments(_measurments: &str) -> i32 {
+fn count_increments(input: &str) -> i32 {
+    let measurements = input.split("\n").collect::<Vec<&str>>();
+
+    if measurements.len() == 2 && measurements[1] > measurements[0] {
+        return 1;
+    }
+
     return 0;
 }
 
@@ -23,10 +29,22 @@ mod tests {
         let output = count_increments("123");
         assert_eq!(output, 0);
     }
+
+    #[test]
+    fn test_two_inputs_incrementing() {
+        let output = count_increments("45\n67");
+        assert_eq!(output, 1);
+    }
+
+    #[test]
+    fn test_two_inputs_decrementing() {
+        let output = count_increments("98\n76");
+        assert_eq!(output, 0);
+    }
 }
 
 pub fn sonar_sweep() {
-    let measurments = read_input();
-    let num_increments = count_increments(&measurments);
+    let measurements = read_input();
+    let num_increments = count_increments(&measurements);
     println!("1.1: {}", num_increments);
 }
