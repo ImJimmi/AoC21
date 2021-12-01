47af3e5c399462fd07863df07cf3956eb3328bf2
James Johnson <james.johnson@focusrite.com>
2021-12-01 12:14:26 +0000

    Test empty input to sonar sweep

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -4,12 +4,23 @@ fn read_input() -> String {
     return fs::read_to_string("./src/day1/input.txt").unwrap();
 }
 
-fn count_increments(_measurments: String) -> i32 {
+fn count_increments(_measurments: &str) -> i32 {
     return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_input() {
+        let output = count_increments(&String::new());
+        assert_eq!(output, 0);
+    }
 }
 
 pub fn sonar_sweep() {
     let measurments = read_input();
-    let num_increments = count_increments(measurments);
+    let num_increments = count_increments(&measurments);
     println!("1.1: {}", num_increments);
 }
