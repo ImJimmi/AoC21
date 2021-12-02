f3308a64d149614830ad542fe20dca4c6c9e4419
James Johnson <james.johnson@focusrite.com>
2021-12-02 10:31:54 +0000

    Test for zero commands

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -6,7 +6,35 @@ fn read_input() -> Vec<String> {
     return tokens.map(|token| token.to_string()).collect();
 }
 
+#[derive(Debug, PartialEq)]
+struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+fn follow_commands_from(_commands: &Vec<String>, initial_position: Position) -> Position {
+    return initial_position;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_zero_commands_from_0_0() {
+        let output = follow_commands_from(&Vec::new(), Position { x: 0, y: 0 });
+        assert_eq!(output, Position { x: 0, y: 0 });
+    }
+
+    #[test]
+    fn test_zero_commands_from_3_2() {
+        let output = follow_commands_from(&Vec::new(), Position { x: 3, y: 2 });
+        assert_eq!(output, Position { x: 3, y: 2 });
+    }
+}
+
 pub fn dive() {
-    let _commands = read_input();
-    println!("2.1: {}", 0);
+    let commands = read_input();
+    let final_position = follow_commands_from(&commands, Position { x: 0, y: 0 });
+    println!("2.1: {:?}", final_position);
 }
