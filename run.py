b5a7b5fb6bd5431238782ac708aa1534ebdc9ea4
James Johnson <james.johnson@focusrite.com>
2021-12-06 11:41:04 +0000

    Add life support rating calculating to binary-diagnostics

--- run.py
+++ run.py
@@ -2,3 +2,4 @@ from src.day3 import binary_diagnostic
 
 if __name__ == '__main__':
     print('3.1:', binary_diagnostic.calculate_power_consumption())
+    print('3.2:', binary_diagnostic.calculate_life_support_rating())
--- src/day3/binary_diagnostic.py
+++ src/day3/binary_diagnostic.py
@@ -1,3 +1,6 @@
+from os import read
+
+
 def read_input():
     with open('src/day3/input.txt') as input_file:
         content = input_file.read().strip()
@@ -15,10 +18,15 @@ def most_common_bit_in_position(readings, position):
         if reading[position] == '1':
             count += 1
 
-    if count > len(readings) / 2:
+    if count >= len(readings) / 2:
         return '1'
 
     return '0'
+
+
+def least_common_bit_in_position(readings, position):
+    most_common = most_common_bit_in_position(readings, position)
+    return '1' if most_common == '0' else '0'
 
 
 def calculate_power_consumption():
@@ -32,3 +40,37 @@ def calculate_power_consumption():
 
     power = int(gamma, 2) * int(epsilon, 2)
     return power
+
+
+def filter_readings(readings, filter):
+    num_bits = len(readings[0])
+    filtered = list(readings)
+
+    for bit in range(num_bits):
+        to_keep = filter(filtered, bit)
+
+        for reading in list(filtered):
+            if reading[bit] != to_keep:
+                filtered.remove(reading)
+
+            if len(filtered) == 1:
+                return int(filtered[0], 2)
+
+    return filtered
+
+
+def calculate_oxygen_rating(readings):
+    return filter_readings(readings, most_common_bit_in_position)
+
+
+def calculate_co2_rating(readings):
+    return filter_readings(readings, least_common_bit_in_position)
+
+
+def calculate_life_support_rating():
+    readings = read_input()
+
+    oxygen_rating = calculate_oxygen_rating(readings)
+    co2_rating = calculate_co2_rating(readings)
+
+    return oxygen_rating * co2_rating
